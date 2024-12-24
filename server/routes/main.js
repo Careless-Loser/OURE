5a3407f9c63ec698ac0c757fd4847d6e0e9dee73
Ifrah Imtiaz <ifrah.imtiaz2612@gmail.com>
2024-12-24 23:45:06 +0400

    Adding Pagination

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,17 +1,45 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post');
+
 
 //Routes
 
 //Index Page
-router.get('', (req, res) => {
-//Index title and description rendering
-    const locals = {
+router.get('', async (req, res) => {
+//Catching the data
+    try{
+        //Index title and description rendering
+        const locals = {
         title: "OURE",
         description: "OURE is a modern blogging platform built with cutting-edge web technologies, offering a seamless and engaging experience for writers and readers alike."
+        }
+
+    //pagination
+    let perPage = 5;
+    let page = req.query.page || 1;
+
+    const data = await Post.aggregate([{ $sort: {createdAt: -1 } }])
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .exec();
+
+    const count = await Post.countDocuments({});
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+        res.render('index', { 
+            locals, 
+            data,
+            current: page,
+            nextPage: hasNextPage ? nextPage : null,
+            currentRoute: '/'
+        });
+
+    } catch (error) {
+        console.log(error);
     }
 
-    res.render('index', { locals });
 }); 
 
 //About Page
@@ -23,7 +51,7 @@ router.get('/about', (req, res) => {
     }
 
     res.render('about', { locals });
-}); 
+});  
 
 //Contact Page
 router.get('/contact', (req, res) => {
@@ -37,3 +65,15 @@ router.get('/contact', (req, res) => {
 }); 
 
 module.exports = router;
+
+// Testing if the database started
+//function insertPostData () {
+    //Post.insertMany([
+
+       // {
+        // title: "Welcome To OURE",
+        //body: "OURE is a personal blogging platform crafted to provide users with a seamless and engaging blogging experience. Whether you're a passionate writer, a storyteller, or someone who simply loves sharing ideas, OURE is here to make your blogging journey effortless and enjoyable. Built with modern web technologies, OURE combines an elegant and user-friendly frontend with a robust and reliable backend. This ensures not only a visually pleasing experience but also a powerful and secure platform to host your thoughts. With OURE, you can: Share your stories and ideas effortlessly. Connect with a growing community of readers and writers. Enjoy a clean, intuitive interface that makes writing and reading a joy. We invite you to explore, write, and connect. Welcome to OURE—a place where your voice finds its home!"
+        //},
+    //])
+//}
+//insertPostData();
